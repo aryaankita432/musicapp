b19962ba23bf98c218873bb685418f8032155ea2
Ankita Arya <aryaankita432@gmail.com>
2022-10-28 11:51:40 +0530

    added homepage elements

--- client/src/components/home.jsx
+++ client/src/components/home.jsx
@@ -1,9 +1,34 @@
 import React from 'react'
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import NavDropdown from 'react-bootstrap/NavDropdown';
+
+
 
 const home = () => {
   return (
-    <div class="gh">homepage</div>
+    
+    <Navbar bg="light" expand="lg">
+      <Container>
+        <Navbar.Brand href="#home">MUSIXON</Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="me-auto">
+          <Nav.Link href="#home">Home</Nav.Link><br></br>
+            <Nav.Link href="#search">Search</Nav.Link><br></br>
+            <Nav.Link href="#library">Your Library</Nav.Link>
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+    
+    
+
+    
   )
 }
 
 export default home
+
+
